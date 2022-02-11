4eb609b9e0e252ca6c2c8ebff053d57e2d0ddede
negin <negin.s.mousavi@gmail.com>
2022-02-11 13:35:15 +0330

    add config to support persian characters

--- src/main/java/ir/maktab/project/config/TomcatConfig.java
+++ src/main/java/ir/maktab/project/config/TomcatConfig.java
@@ -1,4 +1,23 @@
-package ir.maktab.project.config;/**
+package ir.maktab.project.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
  * @author Negin Mousavi
- */public class TomcatConfig {
+ */
+
+@Configuration
+public class TomcatConfig {
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+        tomcatServletWebServerFactory.addConnectorCustomizers((Connector connector) -> {
+            connector.setProperty("relaxedPathChars", "\"{\\}^`{|}");
+            connector.setProperty("relaxedQueryChars", "\"{\\}^`{|}");
+        });
+        return tomcatServletWebServerFactory;
+    }
 }
